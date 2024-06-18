03cfa2bd4566dbdd63a2939bfcc7e1066b9b1a36
Alka Sah <alkasah485@gmail.com>
2024-06-18 11:30:41 -0700

    modification in Thought.js file

--- models/Thought.js
+++ models/Thought.js
@@ -3,12 +3,19 @@ const {Schema, model} = require('mongoose');
 const thoughtSchema = new Schema(
     {
         thoughtText : {
+            type:String,
+            required: true,
+            maxlength: 280,
 
         },
         createdAt: {
+            type:Date,
+            default: Date.now,
 
         },
         username : {
+            type: String,
+            required : true,
 
         }
     }
