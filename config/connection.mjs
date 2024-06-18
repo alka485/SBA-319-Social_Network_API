3020688094365d42e6135dc6af1f0e1748199334
Alka Sah <alkasah485@gmail.com>
2024-06-18 01:46:16 -0700

    modification in connection.js file

--- config/connection.mjs
+++ config/connection.mjs
@@ -1,8 +1,11 @@
 const {connect , connection} = require('mongoose');
-
+// Wrap Mongoose around local connection to MongoDB
 connect('mongodb://localhost/SBA319', {
+    // Set Server Discover and Monitoring engine to true and avoids warning
     useNewUrlParser:true,
     useUnifiedTopology:true,
 });
+
+// Export connection
 
 module.exports = connection;
