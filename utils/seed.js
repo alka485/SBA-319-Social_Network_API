95d1cf0f2a561e2faff35861f3f7eb0095fe7c10
Alka Sah <alkasah485@gmail.com>
2024-06-19 12:09:16 -0700

    modification in seed file

--- utils/seed.js
+++ utils/seed.js
@@ -3,3 +3,29 @@ const {User, Thought} = require('../models');
 const {getRandomName, email, getRandomThought } = require('./data');
 
 connection.on('error' , (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    const thought = getRandomThought(5);
+    const usernames = getRandomName(5);
+    const emails = email;
+    console.log(emails);
+
+    await Thought.collection.insertMany(thought);
+    var thoughts = await Thought.find();
+
+    var users = [];
+
+    for (let i = 0; i < 5; i++) {
+        users.push({
+            usernames: usernames[i],
+            email : emails[i],
+            thoughts:[thoughts[i]._id]
+        })
+
+    }
+    await User.collection.insertMany(users);
+})
