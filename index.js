44598b5f3316bb14e87d9896feea521cf982e88e
Alka Sah <alkasah485@gmail.com>
2024-06-19 12:48:32 -0700

    modification in index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // Import express package
 const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
 const PORT = 4000;
 // Initialize our app variable by setting it to the value of express()
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(routes);
+
 // Listen for connections
-app.listen(PORT, () => {
+db.once('open' , () => {
+    app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}`);
-});
+    });
+})
