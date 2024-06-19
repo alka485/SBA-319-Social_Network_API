6b9178ea2e1601d810db8ce51ce1103ff1528c13
Alka Sah <alkasah485@gmail.com>
2024-06-19 11:29:33 -0700

    modification in data file

---
+++ utils/data.js
@@ -0,0 +1,31 @@
+const username =[
+    'sal',
+    'Lernantino',
+    'Amiko',
+    'Jordan',
+    'Blake',
+ ];
+
+ const email = [
+  'sal@hotmail.com',
+  'lernantino@gmail.com',
+  'amiko2k20@aol.com',
+  'jordan99@msn.com',
+  'the_blake@yahoo.com',
+ ];
+
+ const possibleThoughts = [
+   'Whatever you are,be a good one.',
+   'You do not find the happy life.You make it',
+   'You are stronger than you think',
+   'If you want to become big, stop thinking small.',
+   'One small thought in the morning can change your whole day',
+ ];
+
+ const possibleReactions =[
+         'Smile',
+         'Like',
+         'Angry',
+         'Unlike',
+         'Hate'
+ ];
